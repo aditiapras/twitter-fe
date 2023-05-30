2a296401296c1e69d1e12f041bb20b2d7ecdc3e1
Aditia Prasetian <aditiapras97@gmail.com>
2023-05-30 12:01:01 +0700

    fix replies tweet

--- components/timeline/actionButton.js
+++ components/timeline/actionButton.js
@@ -6,20 +6,20 @@ import { RiBookmarkLine } from "react-icons/ri";
 
 export default function ActionButton() {
   return (
-    <div className="flex justify-between items-start px-10 py-2 border-b border-t border-slate-600">
-      <div className="group flex w-8 h-8 rounded-full hover:bg-slate-700 items-center justify-center">
+    <div className="flex justify-between items-start px-10 py-1 border-b border-t border-slate-600">
+      <div className="group flex w-10 h-10 rounded-full hover:bg-sky-900/40 items-center justify-center">
         <TbMessageCircle2 className="text-slate-500 group-hover:text-sky-500 text-xl"></TbMessageCircle2>
       </div>
-      <div className="group flex w-8 h-8 rounded-full hover:bg-slate-700 items-center justify-center">
+      <div className="group flex w-10 h-10 rounded-full hover:bg-emerald-900/40 items-center justify-center">
         <AiOutlineRetweet className="text-slate-500 group-hover:text-emerald-500 text-xl"></AiOutlineRetweet>
       </div>
-      <div className="group flex w-8 h-8 rounded-full hover:bg-slate-700 items-center justify-center">
+      <div className="group flex w-10 h-10 rounded-full hover:bg-pink-900/40 items-center justify-center">
         <HiOutlineHeart className="text-slate-500 group-hover:text-pink-500 text-xl"></HiOutlineHeart>
       </div>
-      <div className="group flex w-8 h-8 rounded-full hover:bg-slate-700 items-center justify-center">
+      <div className="group flex w-10 h-10 rounded-full hover:bg-sky-900/40 items-center justify-center">
         <RiBookmarkLine className="text-slate-500 group-hover:text-sky-500 text-xl"></RiBookmarkLine>
       </div>
-      <div className="group flex w-8 h-8 rounded-full hover:bg-slate-700 items-center justify-center">
+      <div className="group flex w-10 h-10 rounded-full hover:bg-sky-900/40 items-center justify-center">
         <FiShare className="text-slate-500 group-hover:text-sky-500 text-xl"></FiShare>
       </div>
     </div>
--- components/timeline/createReply.js
+++ components/timeline/createReply.js
@@ -1,20 +1,32 @@
 "use client";
 import TextareaAutosize from "react-textarea-autosize";
+import { useState } from "react";
 
 export default function CreateReply() {
+  const [option, setOption] = useState(false);
   return (
     <>
       <div className="flex gap-3 items-center">
-        <div>
+        <div className="flex h-full">
           <div className="h-12 w-12 bg-slate-600 rounded-full"></div>
         </div>
-        <TextareaAutosize
-          placeholder="Tweet your reply!"
-          className="p-3 outline-none resize-none bg-transparent text-white text-xl w-full"
-          onClick={() => {
-            console.log("clicked");
-          }}
-        />
+        <div className="flex flex-col w-full gap-2">
+          <h1 className={`${option ? "text-slate-500 text-sm" : "hidden"}`}>
+            replying to <span className="text-sky-500">@aditiapras</span>
+          </h1>
+          <TextareaAutosize
+            placeholder="Tweet your reply!"
+            className="outline-none resize-none bg-transparent text-white text-xl w-full"
+            onClick={() => {
+              setOption(true);
+              console.log(option);
+            }}
+          />
+
+          <div className={`flex justify-between items-center`}>
+            <h1 className={`${option ? "text-white" : "hidden"}`}>icon</h1>
+          </div>
+        </div>
         <button className="px-4 h-10 bg-sky-500 hover:bg-sky-500/80 rounded-full text-white">
           Reply
         </button>
